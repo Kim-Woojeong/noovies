5415bd494fc64fbcb99ab60cf77db91f03c3a02b
Kim-Woojeong <kyj990307@naver.com>
2022-11-12 20:38:40 +0900

    1.3 Splash Screen

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Ionicons } from "@expo/vector-icons";
+import * as SplashScreen from 'expo-splash-screen';
+import { Text, View } from 'react-native';
+import * as Font from 'expo-font';
+
+SplashScreen.preventAutoHideAsync();
+
+const loadFonts = fonts => fonts.map(font => Font.loadAsync(font));
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // await Font.loadAsync(Entypo.font);
+        const fonts = loadFonts([Ionicons.font]);
+        await Promise.all([...fonts]);
+
+        await new Promise(resolve => setTimeout(resolve, 2000)); // 2초뒤에 이 promise를 resolve한다고 하자.
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync(); // 숨기기
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+      onLayout={onLayoutRootView}>
+      <Text>SplashScreen Demo! 👋</Text>
+      <Ionicons name="rocket" size={24} color="black" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
