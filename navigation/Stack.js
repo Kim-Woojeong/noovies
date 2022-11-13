6f4e003c365dac795bbaeb92c2d79c764ee3656a
Kim-Woojeong <kyj990307@naver.com>
2022-11-14 01:24:40 +0900

    1.14 Styled Component

--- navigation/Stack.js
+++ navigation/Stack.js
@@ -2,13 +2,11 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Text, TouchableOpacity } from "react-native";
 
-const ScreenOne = ({navigation: {navigate}}) => (
-    // 같은 네비게이터에 있을때
+const ScreenOne = ({navigation: {navigate}}) => (   
   <TouchableOpacity onPress={() => navigate("Two")}>
     <Text>go to Two</Text>
   </TouchableOpacity>
   );
-  // 같은 네비게이터에 있을때
 const ScreenTwo = ({navigation: {navigate}}) => (
   <TouchableOpacity onPress={() => navigate("Three")}>
     <Text>go to Three</Text>
@@ -16,7 +14,6 @@ const ScreenTwo = ({navigation: {navigate}}) => (
   );
 
 const ScreenThree = ({navigation: {navigate}}) => (
-    // 다른 네비게이터일 때
   <TouchableOpacity onPress={()=> navigate("Tabs", {screen:"Search"})}>
     <Text>Change Title</Text>
   </TouchableOpacity>
--- screens/Movies.js
+++ screens/Movies.js
@@ -1,13 +1,22 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+// import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
 
-const Movies = ({navigation: {navigate}}) => (
-  <TouchableOpacity 
-    // 다른 네비게이터일 때
-    onPress={()=> navigate("Stack", {screen:"Three"})}
-    style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-    <Text>Movies</Text>
-  </TouchableOpacity>
+const Btn = styled.TouchableOpacity` 
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  `;
+
+const Title = styled.Text`
+  color: ${ (props) => (props.selected ? "blue" : "red")};
+  `;
+
+const Movies = ({ navigation: {navigate}}) => ( // Btn과 Title로 컴포넌트 변경
+  <Btn onPress={()=> navigate("Stack", {screen: "Three"})}>
+    <Title selected={false}>Movies</Title>
+    <Title selected={true}>Movies</Title>
+  </Btn>
 );
 
 export default Movies;
