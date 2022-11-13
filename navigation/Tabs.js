194cf0258a37e28be65092834ef9323375bc955e
Kim-Woojeong <kyj990307@naver.com>
2022-11-13 10:10:11 +0900

    1.7 Dark Mode

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -3,21 +3,29 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
+import { useColorScheme } from "react-native";
+import { BLACK_COLOR, DARK_GREY, LIGHT_GREY, YELLOW_COLOR } from "../colors";
 
 const Tab = createBottomTabNavigator();
 
-const Tabs = () => (
-    <Tab.Navigator
-        screenOptions={ {
-            tabBarStyle: { backgroundColor: "tomato"}, // 탭의 배경색
-            tabBarActiveTintColor: 'red', // 클릭된 탭
-            tabBarInactiveTintColor: 'purple', // 클릭되지않은 탭
-            headerTitleStyle: {color: "tomato"}, // 헤더 글씨색
-        }}>
-      <Tab.Screen name="Movies" component={Movies} />
-      <Tab.Screen name="Tv" component={Tv} /> 
-      <Tab.Screen name="Search" component={Search} />
-    </Tab.Navigator>
-);
+const Tabs = () => {
+
+    const isDark = useColorScheme() === "dark"; // dark와 light를 반환받는 대신 t/f를 받자.
+
+    return ( // color scheme에 접근해야해서 return을 넣음.
+        <Tab.Navigator
+            screenOptions={ {
+                tabBarStyle: { backgroundColor: isDark ? BLACK_COLOR : "white"}, // 탭의 배경색
+                tabBarActiveTintColor: isDark ? YELLOW_COLOR : BLACK_COLOR, // 클릭된 탭
+                tabBarInactiveTintColor: isDark ? DARK_GREY : LIGHT_GREY, // 클릭되지않은 탭
+                headerStyle: { backgroundColor: isDark ? BLACK_COLOR : "white"},
+                headerTitleStyle: {color: isDark ? "white" : BLACK_COLOR}, // 헤더 글씨색
+            }}>
+        <Tab.Screen name="Movies" component={Movies} />
+        <Tab.Screen name="Tv" component={Tv} /> 
+        <Tab.Screen name="Search" component={Search} />
+        </Tab.Navigator>
+    );
+};
 
 export default Tabs; 
