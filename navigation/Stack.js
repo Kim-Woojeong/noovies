f0afc973d2e604740ca18b70d9948a468382c655
Kim-Woojeong <kyj990307@naver.com>
2022-11-13 23:37:49 +0900

    2.12 Stacks and Tabs

--- navigation/Stack.js
+++ navigation/Stack.js
@@ -1,38 +1,38 @@
 import React from "react";
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Text, TouchableOpacity, View } from 'react-native';
-import { YELLOW_COLOR } from "../colors";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Text, TouchableOpacity } from "react-native";
 
 const ScreenOne = ({navigation: {navigate}}) => (
-    <TouchableOpacity onPress={() => navigate("Two")}>
-        <Text>Go to Two</Text>
-    </TouchableOpacity>
-    );
+    // 같은 네비게이터에 있을때
+  <TouchableOpacity onPress={() => navigate("Two")}>
+    <Text>go to Two</Text>
+  </TouchableOpacity>
+  );
+  // 같은 네비게이터에 있을때
 const ScreenTwo = ({navigation: {navigate}}) => (
-    <TouchableOpacity onPress={() => navigate("Three")}>
-        <Text>Go to Three</Text>
-    </TouchableOpacity>
-    );
-const ScreenThree = ({navigation: {setOptions}}) => (
-    <TouchableOpacity onPress={() => setOptions( {title : "Hello!"})}>
-        <Text>Change Title</Text>
-    </TouchableOpacity>
-    );
+  <TouchableOpacity onPress={() => navigate("Three")}>
+    <Text>go to Three</Text>
+  </TouchableOpacity>
+  );
+
+const ScreenThree = ({navigation: {navigate}}) => (
+    // 다른 네비게이터일 때
+  <TouchableOpacity onPress={()=> navigate("Tabs", {screen:"Search"})}>
+    <Text>Change Title</Text>
+  </TouchableOpacity>
+  );
 
 const NativeStack = createNativeStackNavigator();
 
-const Stack = () => (
+const Stack = () =>
     <NativeStack.Navigator
-        screenOptions={{
-            presentation: "modal",
-            animation: "fade",
-            headerTintColor: YELLOW_COLOR,
-            headerBackTitleVisible: false,
-        }}>
+      screenOptions = {{
+        headerBackTitleVisible: false,
+      }}
+      >
       <NativeStack.Screen name="One" component={ScreenOne} />
       <NativeStack.Screen name="Two" component={ScreenTwo} />
       <NativeStack.Screen name="Three" component={ScreenThree} />
     </NativeStack.Navigator>
-);
 
 export default Stack;
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -5,45 +5,62 @@ import Tv from "../screens/Tv";
 import Search from "../screens/Search";
 import { useColorScheme } from "react-native";
 import { BLACK_COLOR, DARK_GREY, LIGHT_GREY, YELLOW_COLOR } from "../colors";
-import { Ionicons } from '@expo/vector-icons'; 
+import { Ionicons } from "@expo/vector-icons";
+import Stack from "./Stack";
 
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
-
-    const isDark = useColorScheme() === "dark"; // dark와 light를 반환받는 대신 t/f를 받자.
-
-    return ( // color scheme에 접근해야해서 return을 넣음.
-        <Tab.Navigator
-            screenOptions={ {
-                tabBarStyle: { backgroundColor: isDark ? BLACK_COLOR : "white"}, // 탭의 배경색
-                tabBarActiveTintColor: isDark ? YELLOW_COLOR : BLACK_COLOR, // 클릭된 탭
-                tabBarInactiveTintColor: isDark ? DARK_GREY : LIGHT_GREY, // 클릭되지않은 탭
-                headerStyle: { backgroundColor: isDark ? BLACK_COLOR : "white"},
-                headerTitleStyle: {color: isDark ? "white" : BLACK_COLOR}, // 헤더 글씨색
-                tabBarLabelStyle: {
-                    marginTop: -5,
-                    fontSize: 10,
-                    fontWeight: "600",
-                }
-            }}>
-        <Tab.Screen name="Movies" component={Movies} options={{
-            tabBarIcon: ({ color, size }) => (
-                <Ionicons name="film-outline" size={size} color={color} />
-            ),
-        }}/>
-        <Tab.Screen name="Tv" component={Tv} options={{
-            tabBarIcon: ({ color, size }) => (
-                <Ionicons name="tv-outline" size={size} color={color} />
-            ),
-        }}/>
-        <Tab.Screen name="Search" component={Search} options={{
-            tabBarIcon: ({ color, size }) => (
-                <Ionicons name="search-outline" size={size} color={color} />
-            ),
-        }}/>
-        </Tab.Navigator>
-    );
+  const isDark = useColorScheme() === "dark";
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: isDark ? BLACK_COLOR : "white",
+        },
+        tabBarActiveTintColor: isDark ? YELLOW_COLOR : BLACK_COLOR,
+        tabBarInactiveTintColor: isDark ? DARK_GREY : LIGHT_GREY,
+        headerStyle: {
+          backgroundColor: isDark ? BLACK_COLOR : "white",
+        },
+        headerTitleStyle: {
+          color: isDark ? "white" : BLACK_COLOR,
+        },
+        tabBarLabelStyle: {
+          marginTop: -5,
+          fontSize: 10,
+          fontWeight: "600",
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Movies"
+        component={Movies} // Movies대신에 Stack Navigation 렌더링하기
+        options={{
+          tabBarIcon: ({ color, size }) => ( 
+            <Ionicons name={"film-outline"} color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="TV"
+        component={Tv}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="tv-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name={"search-outline"} color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
 };
-
-export default Tabs; 
+export default Tabs;
