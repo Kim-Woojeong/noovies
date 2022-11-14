dc28b8d0e0c42387b790a7dfedaaad6ee8cf9e6e
Kim-Woojeong <kyj990307@naver.com>
2022-11-14 19:36:09 +0900

    2.1 Structure

--- App.js
+++ App.js
@@ -9,7 +9,6 @@ import Stack from './navigation/Stack';
 import Root from './navigation/Root';
 import { ThemeProvider } from "styled-components/native";
 import { darkTheme, lightTheme } from './styled';
-// SplashScreen.preventAutoHideAsync();
 
 const loadFonts = fonts => fonts.map(font => Font.loadAsync(font));
 
@@ -22,7 +21,7 @@ export default function App() {
         const fonts = loadFonts([Ionicons.font]);
         await Promise.all([...fonts]);
 
-        await new Promise(resolve => setTimeout(resolve, 2000)); // 2초뒤에 이 promise를 resolve한다고 하자.
+        await new Promise(resolve => setTimeout(resolve, 2000));
       } catch (e) {
         console.warn(e);
       } finally {
@@ -35,18 +34,17 @@ export default function App() {
 
   const onLayoutRootView = useCallback(async () => {
     if (appIsReady) {
-      await SplashScreen.hideAsync(); // 숨기기
+      await SplashScreen.hideAsync();
     }
   }, [appIsReady]);
 
-  const isDark = useColorScheme() === "dark"; // hook은 한번만 쓰면됨!
+  const isDark = useColorScheme() === "dark";
 
   if (!appIsReady) {
     return null;
   }
 
   return (
-    // ThemeProvider라는 새로운 컴포넌트를 사용해서 테마를 적용해보자. props로 theme을 내려주면 모든 스크린에서 theme에 접근할수있음.
     <ThemeProvider theme = {isDark ? darkTheme : lightTheme}>
       <NavigationContainer >
         <Root />
--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -2,26 +2,6 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
 import styled from "styled-components/native";
 
-// 이 파일에서 다크모드인지 아닌지를 판단할 필요없음!
-
-const Btn = styled.TouchableOpacity` 
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => props.theme.mainBgColor};
-  `;
-
-const Title = styled.Text`
-  color: ${(props) => props.theme.textColor};
-  `;
-
-// Btn과 Title로 컴포넌트 변경
-const Movies: React.FC<NativeStackScreenProps<any, "Movie">> = ({ 
-  navigation: {navigate}
-}) => (
-  <Btn onPress={()=> navigate("Stack", {screen: "Three"})}>
-    <Title>Movies</Title>
-  </Btn>
-);
+const Movies: React.FC<NativeStackScreenProps<any, "Movie">> = () => null;
 
 export default Movies;
