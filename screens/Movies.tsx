3c2283928e4c1516d9c76b13728458409b860d9e
Kim-Woojeong <kyj990307@naver.com>
2022-11-21 20:21:14 +0900

    2.7 Coming Soon

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -45,6 +45,37 @@ const Votes = styled.Text`
   font-size: 10px;
 `;
 
+const ListContainer = styled.View`
+  margin-bottom: 40px;
+`;
+
+const ComingSoonTitle = styled(ListTitle)`
+  margin-bottom: 30px;
+`;
+
+const HMovie = styled.View`
+  flex-direction: row;
+  margin-bottom: 30px;
+  padding: 0px 30px;
+`;
+
+const HColumn = styled.View`
+  margin-left: 15px;
+  width: 80%;
+`;
+
+const Release = styled.Text`
+  color: white;
+  font-size: 12px;
+  margin-vertical: 10px;
+`;
+
+const Overview = styled.Text`
+  color: white;
+  opacity: 0.8;
+  width: 80%;
+`;
+
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
 
   const [loading, setLoading] = useState(true);
@@ -75,7 +106,7 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
   const getUpcoming = async () => {
     const {results} = await (
       await fetch(
-        `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1&region=KR`
+        `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`
         )
     ).json();
     setUpcoming(results);
@@ -104,7 +135,7 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
           autoplayTimeout={3.5}
           showsButtons={false}
           showsPagination={false}
-          containerStyle={{ marginBottom: 30, width: "100%", height: SCREEN_HEIGHT / 4 }}>
+          containerStyle={{ marginBottom: 40, width: "100%", height: SCREEN_HEIGHT / 4 }}>
             {nowPlaying.map((movie) => (
               <Slide
                 key={movie.id}
@@ -116,23 +147,45 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
                 />
             ))}
       </Swiper>
-      <ListTitle>Trending Movie</ListTitle>
-      <TrendingScroll 
-        contentContainerStyle={{ paddingLeft: 30 }}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-      >
-        {trending.map((movie) => (
-          <Movie key={movie.id}>
-            <Poster path={movie.poster_path}/>
-            <Title>
-              {movie.original_title.slice(0, 13)}
-              {movie.original_title.length > 13 ? "..." : null}
-            </Title>
-            <Votes>⭐️ {movie.vote_average}/10</Votes>
-          </Movie>
-          ))}
-      </TrendingScroll>
+
+      <ListContainer>
+          <ListTitle>Trending Movie</ListTitle>
+          <TrendingScroll 
+            contentContainerStyle={{ paddingLeft: 30 }}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+          >
+            {trending.map((movie) => (
+              <Movie key={movie.id}>
+                <Poster path={movie.poster_path}/>
+                <Title>
+                  {movie.original_title.slice(0, 13)}
+                  {movie.original_title.length > 13 ? "..." : null}
+                </Title>
+                <Votes>⭐️ {movie.vote_average}/10</Votes>
+              </Movie>
+              ))}
+          </TrendingScroll>
+        </ListContainer>
+        <ComingSoonTitle>Coming Soon</ComingSoonTitle>
+          {upcoming.map((movie) => (
+            <HMovie key={movie.id}>
+              <Poster path={movie.poster_path}/>
+              <HColumn>
+                <Title>{movie.original_title}</Title>
+                <Release>{new Date(movie.release_date).toLocaleDateString("ko", {
+                  month: "long",
+                  day: "numeric",
+                  year: "numeric",
+                })}</Release>
+                <Overview>
+                  {movie.overview !== "" && movie.overview.length > 80
+                   ? `${movie.overview.slice(0, 140)}...`
+                  : movie.overview}
+                </Overview>
+              </HColumn>
+          </HMovie>
+        ))}
     </Container>
   );
 };
